cc585f0ec1400441e9bf0df6b52bf48189c00146
Toni Schmidbauer <toni@stderr.at>
2014-03-15 09:59:05 +0100

    code cleanup for service mgmt

--- manifests/service/redhat.pp
+++ manifests/service/redhat.pp
@@ -2,31 +2,6 @@
 #
 # Manage tsm service on redhat
 #
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { sysdoc:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
 # === Authors
 #
 # Toni Schmidbauer <toni@stderr.at>
@@ -44,8 +19,7 @@ class tsm::service::redhat {
     group   => 'root',
     mode    => '0755',
     source  => $::tsm::service_script_source,
-  }
-
+  } ->
   service { $::tsm::service_name:
     ensure     => $::tsm::service_ensure,
     enable     => $::tsm::service_enable,
@@ -53,6 +27,4 @@ class tsm::service::redhat {
     hasstatus  => true,
     hasrestart => true,
   }
-
-  File[$::tsm::service_script] -> Service[$::tsm::service_name]
 }
--- manifests/service/solaris.pp
+++ manifests/service/solaris.pp
@@ -2,29 +2,9 @@
 #
 # Manage the tsm service on solaris
 #
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
 # === Examples
 #
-#  class { sysdoc:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  class { tsm::service::solaris:
 #  }
 #
 # === Authors
@@ -58,5 +38,4 @@ class tsm::service::solaris inherits tsm {
     enable   => $::tsm::service_enable,
     manifest => $tsm::service_manifest,
   }
-
 }
