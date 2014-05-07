a9bb37e4867990ed10327793b62ee63dca6610e0
Toni Schmidbauer <toni@stderr.at>
2014-05-07 14:29:01 +0200

    fixed rspec tests for solaris 11 support

--- spec/classes/tsm_spec.rb
+++ spec/classes/tsm_spec.rb
@@ -7,7 +7,8 @@ describe 'tsm' do
     let :facts do
       {
         :osfamily      => system,
-        :kernelrelease => '5.10'
+        :kernelrelease => '5.10',
+        :hardwareisa   => 'i386',
       }
     end
 
@@ -66,11 +67,12 @@ describe 'tsm' do
     end
   end
 
-  describe 'tsm::install on Solaris 10' do
+  describe 'tsm::install on Solaris 10 i386' do
     let :facts do
       {
         :osfamily      => 'Solaris',
         :kernelrelease => '5.10',
+        :hardwareisa   => 'i386',
       }
     end
 
@@ -87,19 +89,53 @@ describe 'tsm' do
                                                          'adminfile' => '/var/sadm/install/admin/puppet',
                                                        })
     end
+
+    it { should contain_package('TIVsmCba').that_requires('Package[TIVsmCapi]') }
+    it { should contain_package('TIVsmCapi').that_requires('Package[gsk8ssl64]') }
+    it { should contain_package('gsk8ssl64').that_requires('Package[gsk8cry64]') }
+    it { should contain_package('gsk8cry64').that_requires('Package[gsk8ssl32]') }
+    it { should contain_package('gsk8ssl32').that_requires('Package[gsk8cry32]') }
   end
+
+  describe 'tsm::install on Solaris 10 sparc' do
+    let :facts do
+      {
+        :osfamily      => 'Solaris',
+        :kernelrelease => '5.10',
+        :hardwareisa   => 'sparc',
+      }
+    end
+
+    it do
+      should contain_tsm__installpkg('TIVsmCba').with({
+                                                         'uri'    => /^http:\/\/.*/,
+                                                         'adminfile' => '/var/sadm/install/admin/puppet',
+                                                       })
+    end
+
+    it do
+      should contain_tsm__installpkg('TIVsmCapi').with({
+                                                         'uri'    => /^http:\/\/.*/,
+                                                         'adminfile' => '/var/sadm/install/admin/puppet',
+                                                       })
+    end
+
+    it { should contain_package('TIVsmCba').that_requires('Package[TIVsmCapi]') }
+    it { should contain_package('TIVsmCapi').that_requires('Package[gsk8ssl64]') }
+    it { should contain_package('gsk8ssl64').that_requires('Package[gsk8cry64]') }
+   end
 
   context 'tsm::service on Solaris 10' do
     let :facts do
       {
         :osfamily      => 'Solaris',
         :kernelrelease => '5.10',
-        :architecure   => 'i386',
+        :hardwareisa   => 'i386',
       }
     end
 
     describe 'when tsm::service_manage is false' do
-      it { should_not contain_class('tsm::service::redhat')}
+      it { should_not contain_class('tsm::service::solaris')}
     end
 
     describe 'when tsm::service_manage is true' do
