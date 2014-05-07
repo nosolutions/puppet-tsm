e992777f9c537a024d411e88cae1f897608fca61
Toni Schmidbauer <toni@stderr.at>
2014-05-07 14:26:04 +0200

    documentation update, inclexcl fixes, solaris 11 support
    introduced a separate option inclexcl_replace, if we should replace the
    include/exclude file.
    
    Solaris 11 is now support, as we install the Solaris 10 packages. There
    are currently no package for Solaris 11 available.

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,99 @@
 #
 # Default parameters for tsm
 #
+# === Variables
+#
+# [*package_ensure*]
+#   Package state
+#   Default: 'installed'
+#
+# [*service_manage*]
+#   Should we manage the tsm scheduler service
+#   Default: false
+#
+# [*service_ensure*]
+#   If we manage the service, what state do we want?
+#   Default: running
+#
+# [*service_enable*]
+#   Should the service start on boot
+#   Default: true
+#
+# [*tsm_host*]
+#   TSM Server to use
+#   Default: unknown
+#
+# [*tsm_port*]
+#   TCP port on the server to connect to
+#   Default: unknown
+#
+# [*config*]
+#   Path to dsm.sys
+#   Default: /opt/tivoli/tsm/client/ba/bin/dsm.sys
+#
+# [*config_template*]
+#   Which template to use for dsm.sys
+#   Default: tsm/config.sys.erb
+#
+# [*config_replace*]
+#   Should we replace dsm.sys
+#   Default: false
+#
+# [*inclexcl*]
+#   Path to the include/exclude file
+#   Default: /opt/tivoli/tsm/client/ba/bin/InclExcl
+#
+# [*inclexcl_replace*]
+#   Should we replace the include/exclude file
+#   Default: false
+#
+# [*packages*]
+#   Packages to be installed
+#   Default:
+#     Redhat: TIVsm-BA
+#     Solaris i386: gsk8cry32,gsk8cry64,gsk8ssl32,gsk8ssl64,TIVsmCapi,TIVsmCba
+#     Solaris sparc: gsk8cry64,gsk8ssl64,TIVsmCapi,TIVsmCba
+#
+# [*package_uri*]
+#   HTTP URI where to find the package, required for Solaris
+#   Default: http://server/pkgs/solaris/${::hardwareisa}/5.10
+#
+# [*package_adminfile*]
+#   Solaris SysV package admin file, required for an unattended installation
+#   Default: /var/sadm/install/admin/puppet
+#
+# [*service_name*]
+#   Name of the TSM service to managen
+#   Default:
+#     RedHat: dsmsched
+#     Solaris: tsm
+#
+# [*service_manifest*]
+#   Solaris SMF manifiest
+#   Default: /var/svc/manifest/site/tsmsched.xml
+#
+# [*service_manifest_source*]
+#   Solaris SMF manifest source to be imported
+#   Default: puppet:///modules/tsm/tsmsched.xml
+#
+# [*service_script*]
+#   Script for restarting the TSM Scheduler
+#   Default:
+#     RedHat: /etc/init.d/dsmsched
+#     Solaris: /lib/svc/method/tsmsched
+#
+# [*serivce_script_source*]
+#   Script source for restarting the TSM Scheduler
+#   Default:
+#     RedHat: puppet:///modules/tsm/dsmsched.redhat
+#     Solaris: puppet:///modules/tsm/tsmsched.solaris
+#
+# [*inclexcl_source*]
+#   Source for the include/exclude file
+#   Default:
+#     RedHat: puppet:///modules/tsm/InclExcl.redhat
+#     Solaris: puppet:///modules/tsm/InclExcl.solaris
+#
 # === Authors
 #
 # Toni Schmidbauer <toni@stderr.at>
@@ -24,7 +117,8 @@ class tsm::params {
   $config_template = 'tsm/dsm.sys.erb'
   $config_replace  = false
 
-  $inclexcl       = '/opt/tivoli/tsm/client/ba/bin/InclExcl'
+  $inclexcl         = '/opt/tivoli/tsm/client/ba/bin/InclExcl'
+  $inclexcl_replace = false
 
   case $::osfamily {
     redhat: {
@@ -35,10 +129,10 @@ class tsm::params {
       $inclexcl_source       = 'puppet:///modules/tsm/InclExcl.redhat'
     }
     solaris: {
-      case $::kernelrelease {
-        5.10: {
+      case $::hardwareisa {
+        i386: {
           $packages                = ['gsk8cry32','gsk8cry64','gsk8ssl32','gsk8ssl64','TIVsmCapi', 'TIVsmCba']
-          $package_uri             = "http://sunkist6.eb.lan.at/pkgs/solaris/${::hardwareisa}/5.10"
+          $package_uri             = "http://server/pkgs/solaris/${::hardwareisa}/5.10"
           $package_adminfile       = '/var/sadm/install/admin/puppet'
           $service_name            = 'tsm'
           $service_manifest        = '/var/svc/manifest/site/tsmsched.xml'
@@ -47,11 +141,19 @@ class tsm::params {
           $service_script_source   = 'puppet:///modules/tsm/tsmsched.solaris'
           $inclexcl_source         = 'puppet:///modules/tsm/InclExcl.solaris'
         }
-        5.11: {
-          fail("Solaris 11 is currently not supported")
+        sparc: {
+          $packages                = ['gsk8cry64','gsk8ssl64','TIVsmCapi', 'TIVsmCba']
+          $package_uri             = "http://server/pkgs/solaris/${::hardwareisa}/5.10"
+          $package_adminfile       = '/var/sadm/install/admin/puppet'
+          $service_name            = 'tsm'
+          $service_manifest        = '/var/svc/manifest/site/tsmsched.xml'
+          $service_manifest_source = 'puppet:///modules/tsm/tsmsched.xml'
+          $service_script          = '/lib/svc/method/tsmsched'
+          $service_script_source   = 'puppet:///modules/tsm/tsmsched.solaris'
+          $inclexcl_source         = 'puppet:///modules/tsm/InclExcl.solaris'
         }
         default:{
-          fail("Unsupported kernelrelease ${::kernelrelease} for osfamily ${::osfamily} in config.pp!")
+          fail("Unsupported hardwareisa ${::hardwareisa} for osfamily ${::osfamily} in config.pp!")
         }
       }
     }
