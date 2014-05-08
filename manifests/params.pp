1654b3f2f22c43fd7496ca8e9a2dc3a37971912e
Toni Schmidbauer <toni@stderr.at>
2014-05-08 21:01:09 +0200

    fixed pkg uri and the failure message

--- manifests/params.pp
+++ manifests/params.pp
@@ -133,7 +133,7 @@ class tsm::params {
       case $::hardwareisa {
         i386: {
           $packages                = ['gsk8cry32','gsk8cry64','gsk8ssl32','gsk8ssl64','TIVsmCapi', 'TIVsmCba']
-          $package_uri             = "http://sunkist6.eb.lan.at/pkgs/solaris/${::hardwareisa}/5.10"
+          $package_uri             = "http://server/pkgs/solaris/${::hardwareisa}/5.10"
           $package_adminfile       = '/var/sadm/install/admin/puppet'
           $service_name            = 'tsm'
           $service_manifest        = '/var/svc/manifest/site/tsmsched.xml'
@@ -154,7 +154,7 @@ class tsm::params {
           $inclexcl_source         = 'puppet:///modules/tsm/InclExcl.solaris'
         }
         default:{
-          fail("Unsupported kernelrelease ${::kernelrelease} for osfamily ${::osfamily} in config.pp!")
+          fail("Unsupported hardwareisa ${::hardwareisa} for osfamily ${::osfamily} in config.pp!")
         }
       }
     }
