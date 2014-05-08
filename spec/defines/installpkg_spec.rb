b4a1b6a63d5b587733cb7d209c6d0e7dbe90cd59
Toni Schmidbauer <toni@stderr.at>
2014-05-08 20:59:59 +0200

    import rspec tests for tsm::installpkg

---
+++ spec/defines/installpkg_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'tsm::installpkg', :type => :define do
+  let(:title) { 'TIVsm-BA'}
+
+  context 'on Redhat' do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    it { should contain_package('TIVsm-BA').with_ensure('installed') }
+  end
+
+  context 'on Solaris' do
+    let :facts do
+      {
+        :osfamily => 'Solaris'
+      }
+    end
+
+    let :params do
+      {
+        :adminfile => '/adminfile',
+        :uri       => 'http://server/pkg'
+      }
+    end
+
+    it do
+      should contain_package('TIVsm-BA').with({
+                                                'ensure'          => 'installed',
+                                                'source'          => 'http://server/pkg/TIVsm-BA.pkg',
+                                                'adminfile'       => '/adminfile',
+                                                'install_options' => '-G',
+                                                'provider'        => 'sun'
+                                              })
+    end
+  end
+end
