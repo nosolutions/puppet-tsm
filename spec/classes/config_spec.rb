628d025f1cb7a2395d3cc4e8e4ef7e3d438db16f
Toni Schmidbauer <toni@stderr.at>
2016-04-26 20:27:19 +0200

    config_spec for issue #35

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 describe 'tsm::config' do
   on_supported_os.each do |os, facts|
-    let(:pre_condition) { 'include tsm' }
 
+    tsm_class = <<-EOS
+    class { tsm:
+      tcp_server_address => "127.0.0.1",
+      config_hash => { "key" => "value" },
+    }
+    EOS
+
+    let(:pre_condition) { tsm_class }
     context "on #{os}" do
       let(:facts) { facts }
-
       it { is_expected.to compile.with_all_deps }
+      it { should contain_concat__fragment('dsm_sys_template').with_content(/^\s+key\s+value/) }
     end
   end
 end
